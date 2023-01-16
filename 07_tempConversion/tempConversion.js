370fee12483eb1d42dfa0fa5e20d04056b830a67
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2023-01-16 19:10:59 +0000

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  convertedTemp = (temp - 32) * (5 / 9);
+  return roundToOneDP(convertedTemp);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  convertedTemp = temp * (9 / 5) + 32;
+  return roundToOneDP(convertedTemp);
 };
+
+function roundToOneDP(number) {
+  return Math.round(number * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
