3a12bba00de3ca760ee1d4671732a73b8bbf7585
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2023-01-16 19:11:25 +0000

    Exercise 3 second commit, didnt commit before.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  stringArray = string.split("");
+  reversedArray = stringArray.reverse();
+  return reversedArray.join("");
 };
 
 // Do not edit below this line
