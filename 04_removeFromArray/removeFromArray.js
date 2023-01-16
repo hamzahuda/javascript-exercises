becebb5eda2ec98444e8431f15721d1462c4f105
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2023-01-16 18:36:08 +0000

    Complete exercise 3 and 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array) {
+  for (i = 1; i < arguments.length; i++) {
+    index = array.indexOf(arguments[i]);
+    if (index != -1) {
+      array.splice(index, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
