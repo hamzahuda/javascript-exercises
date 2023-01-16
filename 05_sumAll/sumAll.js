8327a84de148efc71192265560fab260ffa5ac20
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2023-01-16 18:45:57 +0000

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (numOne, numTwo) {
+  if (
+    numOne < 0 ||
+    numTwo < 0 ||
+    typeof numOne != "number" ||
+    typeof numTwo != "number"
+  ) {
+    return "ERROR";
+  }
+  if (numOne > numTwo) {
+    n = numOne - numTwo + 1;
+  } else {
+    n = numTwo - numOne + 1;
+  }
+  return (n / 2) * (numOne + numTwo);
 };
 
 // Do not edit below this line
