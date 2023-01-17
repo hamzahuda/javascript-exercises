66d099f148ddcee7604949e5186dfee81922d9de
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2023-01-17 20:01:51 +0000

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  curYear = new Date().getFullYear();
+  let oldestPerson = {
+    age: 0,
+  };
+  people.forEach((person) => {
+    let age;
+    if ("yearOfDeath" in person) {
+      age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+      age = curYear - person.yearOfBirth;
+    }
+    person["age"] = age;
+    if (age > oldestPerson.age) {
+      oldestPerson = person;
+    }
+  });
+  return oldestPerson;
 };
 
 // Do not edit below this line
