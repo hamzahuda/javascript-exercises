bf500f1df67bb09d8d15cf9d4d827ae1eaeb6cf6
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2023-01-17 19:00:29 +0000

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let strArray = str.toLowerCase().split("").filter(isLetter);
+  let reverseStrArray = [...strArray];
+  reverseStrArray.reverse();
+  console.log(strArray, reverseStrArray);
+  return strArray.join("") === reverseStrArray.join("");
 };
+
+function isLetter(c) {
+  return c.toLowerCase() != c.toUpperCase();
+}
 
 // Do not edit below this line
 module.exports = palindromes;
