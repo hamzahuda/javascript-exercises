083ef799a5050b987f4e5ca4f39fb8c4e1186094
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2023-01-17 19:16:40 +0000

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num < 0) {
+    return "OOPS";
+  } else if (num <= 2) {
+    return 1;
+  } else {
+    const fibonacci = [1, 1];
+    for (let i = 2; i < num; i++) {
+      fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2]);
+    }
+    return fibonacci[num - 1];
+  }
 };
 
 // Do not edit below this line
